90b9c9063c6c5e1e8f8901d74f0a933733ef0798
ZhangHong <2374347637@qq.com>
2022-08-21 22:57:59 +0800

    asd

--- src/main/java/zhiyuanzhe/dao/UserDao.java
+++ src/main/java/zhiyuanzhe/dao/UserDao.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface UserDao {
 
     List<UserInfo> findAll();
-    UserInfo findAllUser(UserInfo userInfo);
+    UserInfo findUser(UserInfo userInfo);
     int addUser(UserInfo userInfo);
     int updateUser(UserInfo userInfo);
     int deleteUser(UserInfo userInfo);
