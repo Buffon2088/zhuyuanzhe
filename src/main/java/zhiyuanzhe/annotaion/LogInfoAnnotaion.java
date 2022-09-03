5e4377992bab82d7ae0af47ea04ca34972363c73
Administrator <1986483161@qq.com>
2022-09-03 23:41:25 +0800

    Aop注解

---
+++ src/main/java/zhiyuanzhe/annotaion/LogInfoAnnotaion.java
@@ -0,0 +1,10 @@
+package zhiyuanzhe.annotaion;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LogInfoAnnotaion {
+    String methodName() default "";
+}
